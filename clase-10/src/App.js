817587618ed8fe589aa4f63e85b810abc3b67913
Dario Argañaraz <dario.arganaraz@gmail.com>
2022-10-15 15:20:55 -0300

    Se agrega props al video

--- clase-10/src/App.js
+++ clase-10/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import Audio from './components/audio/audio';
 import Video from './components/video/video';
+import aud from './assets/audio/src_efecto-de-sonido-golpe.mp3';
+import vid from './assets/video/background_video.mp4';
 
 function App() {
   return (
     <div className="App">
       <Navbar nombre="Componente Navbar con React-Bootstrap"/>
-      <Audio fuente="/assets/audio/src_efecto-de-sonido-golpe.mp3"/>
-      <Video />
+      <Audio fuente = {aud}/>
+      <Video fuente = {vid}/>
     </div>
   );
 }
--- clase-10/src/components/video/video.js
+++ clase-10/src/components/video/video.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+/*
 import video from '../../assets/video/background_video.mp4';
 
 export default class Video extends Component {
@@ -6,7 +8,7 @@ export default class Video extends Component {
         return (
             <div>
                 <h2>Video</h2>
-                <video width="auto" height="auto" controls autoplay="true" muted loop>
+                <video width="auto" height="auto" controls autoPlay={true} muted loop>
                     <source src={video} type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
@@ -14,6 +16,18 @@ export default class Video extends Component {
         );
     }
 }
+*/
 
+export default function Video(props) {
+    return (
+        <div>
+            <h2>Video</h2>
+              <video width="auto" height="auto" controls autoplay="true" muted loop>
+                <source src={props.fuente} type="video/mp4" />
+                Your browser does not support the video element.
+            </video>
+        </div>
+    );
+}
 
 
